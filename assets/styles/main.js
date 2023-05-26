530771be9a9ed8d65c977d08f980a0d8364565b0
[00AliNuri] <[alinn@.code.edu.az]>
2023-05-26 13:44:43 +0400

    first navbar added

---
+++ assets/styles/main.js
@@ -0,0 +1,17 @@
+const myArrow = document.getElementById('arrow')
+const myShape = document.querySelector('.shape')
+const myBtn = document.querySelector('.btn')
+let state = 'rotate(180deg)'
+let state1 = '1'
+
+myArrow.addEventListener('click', ()=>{
+    myArrow.style.transform = state
+    state = state == 'rotate(180deg)' ? 'rotate(360deg)' : 'rotate(180deg)'
+})
+
+myBtn.addEventListener('click', ()=>{
+    myShape.style.opacity = state1
+    state1 = state1 == '1' ? '0' : '1'
+    myArrow.style.transform = state
+    state = state == 'rotate(180deg)' ? 'rotate(360deg)' : 'rotate(180deg)'
+})
