ff01bd6ea373b79eab0c9d8152ee9640446c504c
[00AliNuri] <[alinn@.code.edu.az]>
2023-05-29 14:03:13 +0400

    home page sections added and started design

--- assets/styles/main.js
+++ assets/styles/main.js
@@ -1,20 +1,58 @@
-const myArrow = document.getElementById('arrow')
-const myShape = document.querySelector('.shape')
-const myBtn = document.querySelector('.btn')
+const myArrow = document.getElementById("arrow");
+const myShape = document.querySelector(".shape");
+const myBtn = document.querySelector(".btn");
+
+let state = "rotate(180deg)";
+let state1 = "1";
+
+myArrow.addEventListener("click", () => {
+  myArrow.style.transform = state;
+  state = state == "rotate(180deg)" ? "rotate(360deg)" : "rotate(180deg)";
+});
+
+myBtn.addEventListener("click", () => {
+  myShape.style.opacity = state1;
+  state1 = state1 == "1" ? "0" : "1";
+  myArrow.style.transform = state;
+  state = state == "rotate(180deg)" ? "rotate(360deg)" : "rotate(180deg)";
+});
 
 
-let state = 'rotate(180deg)'
-let state1 = '1'
 
-myArrow.addEventListener('click', ()=>{
-    myArrow.style.transform = state
-    state = state == 'rotate(180deg)' ? 'rotate(360deg)' : 'rotate(180deg)'
-})
+window.onload = function () {
+  const prevBtn = document.getElementById("prev");
+  const nextBtn = document.getElementById("next");
 
-myBtn.addEventListener('click', ()=>{
-    myShape.style.opacity = state1
-    state1 = state1 == '1' ? '0' : '1'
-    myArrow.style.transform = state
-    state = state == 'rotate(180deg)' ? 'rotate(360deg)' : 'rotate(180deg)'
-})
+  prevBtn.addEventListener("click", function () {
+    prevSlide();
+  });
+  nextBtn.addEventListener("click", function () {
+    nextSlide();
+  });
+};
+let slideNumber = 0;
+const prevSlide = () => {
+  const slides = document.getElementsByClassName("slides");
+  const slider = document.getElementById("slider");
+  const currentSlide = slider.getElementsByClassName("current");
+  currentSlide[0].classList.remove("current");
+  if (slideNumber == 0) {
+    slideNumber = slides.length - 1;
+  } else {
+    slideNumber = slideNumber - 1;
+  }
+  slides[slideNumber].classList.add("current");
+};
+const nextSlide = () => {
+  const slides = document.getElementsByClassName("slides");
+  const slider = document.getElementById("slider");
+  const currentSlide = slider.getElementsByClassName("current");
+  currentSlide[0].classList.remove("current");
+  if (slideNumber == slides.length - 1) {
+    slideNumber = 0;
+  } else {
+    slideNumber = slideNumber + 1;
+  }
+  slides[slideNumber].classList.add("current");
+};
 
